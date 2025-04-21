8a4a612f61ba0dc68fbab4080927d8153e5d4c02
Facudebug123 <facundochurquina08@gmail.com>
2025-04-20 22:34:05 -0300

    modifPunto8-9

--- js/ejercicio8.js
+++ js/ejercicio8.js
@@ -1,8 +1,10 @@
-let num1 = 7,
-  num2 = 7;
-
-if (num1 > num2) {
-  console.log(`Numero mayor: ${num1}.`);
-} else if (num2 > num1) {
-  console.log(`Numero mayor: ${num2}.`);
-} else console.log(`Son iguales: ${num1}.`);
+const calcularMayor = (num1, num2) => {
+  let may;
+  if (num1 > num2) {
+    may = `Numero mayor: ${num1}.`;
+  } else if (num2 > num1) {
+    may = `Numero mayor: ${num2}.`;
+  } else may = `Son iguales: ${num1}.`;
+  return may;
+};
+console.log(calcularMayor(8, 10));
